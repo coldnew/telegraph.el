16549bb40802db910215e2ada9cb30ab2dd425f3
Yen-Chin Lee <coldnew.tw@gmail.com>
2018-02-28 12:44:45 +0800

    first commit
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

---
+++ telegraph.el
@@ -0,0 +1,47 @@
+;;; telegraph.el --- telegra.ph api for emacs
+
+;; Copyright (c) 2018 Yen-Chin, Lee.
+;;
+;; Author: coldnew <coldnew.tw@gmail.com>
+;; Keywords: converience
+;; X-URL: http://github.com/coldnew/telegraph.el
+;; Version: 0.1
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+;;; Commentary:
+;; [![MELPA](http://melpa.org/packages/telegraph.el.svg)](http://melpa.org/#/telegraph.el)
+;; [![MELPA Stable](http://stable.melpa.org/packages/telegraph.el.svg)](http://stable.melpa.org/#/telegraph.el)
+
+;;; Installation:
+
+;; If you have `melpa` and `emacs24` installed, simply type:
+;;
+;;     M-x package-install telegraph
+;;
+;; And add the following to your .emacs
+;;
+;;     (require 'telegraph)
+
+;;; Code:
+
+(eval-when-compile (require 'cl))
+
+
+
+(provide 'telegraph)
+;;; telegraph.el ends here
